fc46191c1db6e67e84e0f978eb83dee356a1a5a4
Brendan Cazier <520246+cazier@users.noreply.github.com>
2023-05-31 10:49:11 +0000

    Moved options to individual file

---
+++ db/options.go
@@ -0,0 +1,65 @@
+package db
+
+type MariaDBOptions struct {
+	Username string
+	Password string
+	Database string
+	Host     string
+	Port     int
+	Other    string
+
+	LogLevel int
+	LogPath  string
+}
+
+// This sucks
+func (o *MariaDBOptions) validate() {
+	if o.Username == "" {
+		o.Username = "wc"
+	}
+	if o.Password == "" {
+		o.Password = "password"
+	}
+	if o.Host == "" {
+		o.Host = "127.0.0.1"
+	}
+	if o.Port == 0 {
+		o.Port = 3306
+	}
+	if o.Database == "" {
+		o.Database = "wc"
+	}
+	if o.Other == "" {
+		o.Other = "charset=utf8&parseTime=True"
+	}
+	if o.LogLevel == 0 {
+		o.LogLevel = 4
+	}
+	if o.LogPath == "" {
+		o.LogPath = "<stdout>"
+	}
+}
+
+type SqliteDBOptions struct {
+	Path   string
+	Memory bool
+	Other  string
+
+	LogLevel int
+	LogPath  string
+}
+
+func (o *SqliteDBOptions) validate() {
+	if o.Memory {
+		o.Path = ":memory:"
+	}
+	if o.Path == "" {
+		o.Path = "storage/test.db"
+	}
+	if o.LogLevel == 0 {
+		o.LogLevel = 4
+	}
+	if o.LogPath == "" {
+		o.LogPath = "<stdout>"
+	}
+}
